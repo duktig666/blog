790d440a8993e77c7af32e1a039921f544980b49
rsw <1487660836@qq.com>
2020-04-21 13:23:26 +0800

    跨域问题，解决  完成  rsw

--- blog-system/src/main/java/com/blog/config/GlobalCorsConfig.java
+++ blog-system/src/main/java/com/blog/config/GlobalCorsConfig.java
@@ -17,8 +17,8 @@ public class GlobalCorsConfig {
         //1.添加CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
         //1) 允许的域,不要写*，否则cookie就无法使用了
-        config.addAllowedOrigin("http://localhost:8089/");
-        config.addAllowedOrigin("http://localhost:8090/");
+        config.addAllowedOrigin("http://localhost:8090");
+        config.addAllowedOrigin("http://localhost:8089");
         //2) 是否发送Cookie信息
         config.setAllowCredentials(true);
         //3) 允许的请求方式
