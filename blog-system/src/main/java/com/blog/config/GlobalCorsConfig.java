befc9dfa0ddfd5975825d952e7537e50e65dbd92
rsw <1487660836@qq.com>
2020-05-14 20:52:02 +0800

    修改跨域为所有客户机访问2.0 rsw

--- blog-system/src/main/java/com/blog/config/GlobalCorsConfig.java
+++ blog-system/src/main/java/com/blog/config/GlobalCorsConfig.java
@@ -17,9 +17,7 @@ public class GlobalCorsConfig {
         //1.添加CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
         //1) 允许的域,不要写*，否则cookie就无法使用了
-        config.addAllowedOrigin("http://0.0.0.0:8089");
-        config.addAllowedOrigin("http://0.0.0.0:8088");
-        config.addAllowedOrigin("http://0.0.0.0:8080");
+        config.addAllowedOrigin("*");
         //2) 是否发送Cookie信息
         config.setAllowCredentials(true);
         //3) 允许的请求方式
